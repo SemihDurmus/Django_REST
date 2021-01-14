57f16c90e5606865dcf9cb4cf76bbad1de80f0a9
SemihDurmus <semihdurmus@yahoo.com>
2021-01-14 12:31:58 +0100

    Add: student_create_api

--- src/fscohort_api/urls.py
+++ src/fscohort_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_api, student_list_api
+from .views import home_api, student_list_api, student_create_api
 
 urlpatterns = [
     path("home-api/", home_api),
     path("list-api/", student_list_api),
+    path("create-api/", student_create_api),
 ]
--- src/fscohort_api/views.py
+++ src/fscohort_api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from fscohort.models import Student
 from django.core.serializers import serialize
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home_api(request):
@@ -37,10 +39,32 @@ def student_list_api(request):
         students = Student.objects.all()
         student_count = Student.objects.count()
         student_data = serialize("python", students)
-        print(type(student_data))
+
         data = {
             "students": student_data,
             "count": student_count
         }
 
     return JsonResponse(data)
+
+
+@csrf_exempt
+def student_create_api(request):
+    if request.method == "POST":
+        post_body = json.loads(request.body)
+        print(type(post_body))  # <class 'dict'>
+        name = post_body.get("first_name")
+        last_name = post_body.get("last_name")
+        number = post_body.get("number")
+
+        student_data = {
+            "first_name": name,
+            "last_name": last_name,
+            "number": number
+        }
+
+        student_obj = Student.objects.create(**student_data)
+        data = {
+            "message": f"Student {student_obj.first_name} created successfully"
+        }
+        return JsonResponse(data, status=201)
