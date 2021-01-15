b2aac6eafdc6981348eeaa0cad59440abf965cc8
SemihDurmus <semihdurmus@yahoo.com>
2021-01-15 19:46:57 +0100

    Add: url to StudentSerializer

--- src/fscohort_api/serializers.py
+++ src/fscohort_api/serializers.py
@@ -3,6 +3,11 @@ from fscohort.models import Student
 
 
 class StudentSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='detail',
+        lookup_field='id'
+    )
+
     class Meta:
         model = Student
-        fields = ["id", "first_name", "last_name", "number"]
+        fields = ["url", "id", "first_name", "last_name", "number"]
